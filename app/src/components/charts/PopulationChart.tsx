4fb8da883f28f4a55cf066488b0a67f3bc14fa04
KINE-M <a.m.cassandre.k@gmail.com>
2022-04-23 21:20:31 +0900

    refactor: グラフのデザインを修正：x、y軸の文字の大きさ、Gridの表示

--- app/src/components/charts/PopulationChart.tsx
+++ app/src/components/charts/PopulationChart.tsx
@@ -1,6 +1,14 @@
 import { FC } from 'react';
 import { randomColorHex } from '../../util/color';
-import { LineChart, Line, XAxis, YAxis, Legend, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+  CartesianGrid,
+} from 'recharts';
 
 interface PopulationChartProps {
   populationValue: any;
@@ -11,13 +19,13 @@ const PopulationChart: FC<PopulationChartProps> = ({ populationValue, checkedPre
   return (
     <div className="chart-box">
       <div className="chart">
-        <ResponsiveContainer width={'90%'} height={400}>
+        <ResponsiveContainer width={'90%'} height={550}>
           <LineChart
             width={400}
             height={500}
             margin={{
               top: 30,
-              right: 30,
+              right: 5,
               left: 30,
               bottom: 30,
             }}
@@ -27,12 +35,18 @@ const PopulationChart: FC<PopulationChartProps> = ({ populationValue, checkedPre
               checkedPrefList.map((item) => (
                 <Line key={item[1]} type="monotone" dataKey={item[1]} stroke={randomColorHex()} />
               ))}
+            <CartesianGrid strokeDasharray="3 3" />
             <XAxis
+              fontSize={10}
               dataKey="year"
-              label={{ value: '年度', angle: 0, position: 'insideBottomRight', offset: -15 }}
+              label={{ value: '年度', position: 'insideBottomRight', offset: -5 }}
             />
-            <YAxis label={{ value: '人口数', angle: 0, position: 'insideTopLeft', offset: -25 }} />
-            <Legend verticalAlign="bottom" />
+            <YAxis
+              fontSize={12}
+              width={40}
+              label={{ value: '人口数', angle: 0, position: 'insideTopLeft', offset: -20 }}
+            />
+            <Legend fontSize={8} iconSize={8} />
           </LineChart>
         </ResponsiveContainer>
       </div>
