db5896115f57466f02c1af5f5a8e01b935bdd6db
KINE-M <a.m.cassandre.k@gmail.com>
2022-04-23 18:49:51 +0900

    feat: css を適用しレスポンシブに対応

--- app/src/components/charts/PopulationChart.tsx
+++ app/src/components/charts/PopulationChart.tsx
@@ -10,28 +10,32 @@ interface PopulationChartProps {
 const PopulationChart: FC<PopulationChartProps> = ({ populationValue, checkedPrefList }) => {
   return (
     <div className="chart-box">
-      <LineChart
-        width={400}
-        height={500}
-        margin={{
-          top: 30,
-          right: 30,
-          left: 30,
-          bottom: 30,
-        }}
-        data={populationValue}
-      >
-        {checkedPrefList &&
-          checkedPrefList.map((item) => (
-            <Line key={item[1]} type="monotone" dataKey={item[1]} stroke={randomColorHex()} />
-          ))}
-        <XAxis
-          dataKey="year"
-          label={{ value: '年度', angle: 0, position: 'insideBottomRight', offset: -15 }}
-        />
-        <YAxis label={{ value: '人口数', angle: 0, position: 'insideTopLeft', offset: -25 }} />
-        <Legend verticalAlign="bottom" />
-      </LineChart>
+      <div className="chart">
+        <ResponsiveContainer width={'90%'} height={400}>
+          <LineChart
+            width={400}
+            height={500}
+            margin={{
+              top: 30,
+              right: 30,
+              left: 30,
+              bottom: 30,
+            }}
+            data={populationValue}
+          >
+            {checkedPrefList &&
+              checkedPrefList.map((item) => (
+                <Line key={item[1]} type="monotone" dataKey={item[1]} stroke={randomColorHex()} />
+              ))}
+            <XAxis
+              dataKey="year"
+              label={{ value: '年度', angle: 0, position: 'insideBottomRight', offset: -15 }}
+            />
+            <YAxis label={{ value: '人口数', angle: 0, position: 'insideTopLeft', offset: -25 }} />
+            <Legend verticalAlign="bottom" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
